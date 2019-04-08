c39bf6f020fc793d72fc6e1c0820208e0819b10b
Lee Bailey <lbailey@shipchain.io>
2019-04-08 15:11:40 -0400

    fixing deploy script and s3 assets

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@ cp compose/django/requirements.txt Pipfile
 pyenv/bin/pip install -r Pipfile
 
 zappa update
-pyenv/bin/python manage.py collectstatic --noinput
+rm -rf static
+is_zappa=true pyenv/bin/python manage.py collectstatic --noinput
+rm -rf static
